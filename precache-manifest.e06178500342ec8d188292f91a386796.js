1f9aae76d507fe686c494f6e3faebf62fd09b522
Małgorzata Brożek <malgorzata.brozek@softwarehut.com>
2020-05-06 20:06:49 +0200

    Updates

---
+++ precache-manifest.e06178500342ec8d188292f91a386796.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be780fdcdaa7d250792ea8e0482ffe3",
+    "url": "/react-page-with-router/index.html"
+  },
+  {
+    "revision": "5fbea6c725088adf496c",
+    "url": "/react-page-with-router/static/css/main.ea82cdb9.chunk.css"
+  },
+  {
+    "revision": "355a4b93367985b33bb7",
+    "url": "/react-page-with-router/static/js/2.2e5903e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-page-with-router/static/js/2.2e5903e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fbea6c725088adf496c",
+    "url": "/react-page-with-router/static/js/main.de0b78ed.chunk.js"
+  },
+  {
+    "revision": "642ca0bc7f7eec31208e",
+    "url": "/react-page-with-router/static/js/runtime-main.654d1517.js"
+  },
+  {
+    "revision": "dc3ab31a609d9f0488c9151424b9b0f7",
+    "url": "/react-page-with-router/static/media/bird.dc3ab31a.jpg"
+  },
+  {
+    "revision": "8d40490647845161be41fd5a5bc5ca87",
+    "url": "/react-page-with-router/static/media/bridge.8d404906.jpg"
+  },
+  {
+    "revision": "1c66e0d498525503276438be4bfaf6a0",
+    "url": "/react-page-with-router/static/media/cm.1c66e0d4.jpg"
+  },
+  {
+    "revision": "3c976dbb59122e958a3d1207ffbe2063",
+    "url": "/react-page-with-router/static/media/night.3c976dbb.jpg"
+  },
+  {
+    "revision": "841d7bced3fb790a889276305d71df44",
+    "url": "/react-page-with-router/static/media/obi-wan.841d7bce.jpg"
+  },
+  {
+    "revision": "9d21066b55570a1ac0296ab7f673e96a",
+    "url": "/react-page-with-router/static/media/stairs.9d21066b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ce366cdc52741d585bd9717c4cdcfcdb.js"
+  "/react-page-with-router/precache-manifest.e06178500342ec8d188292f91a386796.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-page-with-router/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
